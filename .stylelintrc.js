7f4babfc0654742a1c95cc78a6b7b8a9f6c1f20a
Lirik <melnicukkiril2002@gmail.com>
2025-08-14 17:21:52 +0300

    Updates

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: "@mate-academy/stylelint-config",
+  rules: {
+    "selector-pseudo-class-no-unknown": [true, {
+      ignorePseudoClasses: ["global"],
+    }],
+  },
+};
