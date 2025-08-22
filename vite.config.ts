d084c3c92a71ab9518795236df8d68f5950591c9
Lirik <melnicukkiril2002@gmail.com>
2025-08-22 22:07:28 +0300

    update demolink

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/my_react_phone-catalog/',
-})
+  build: {
+    outDir: 'docs', // <- білд у docs замість dist
+  },
+});
